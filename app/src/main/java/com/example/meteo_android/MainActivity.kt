383d715c62fbddc0bc686ccea96e94d18abf691f
Kristaps <kbebris+git@gmail.com>
2024-08-04 16:38:57 +0300

    learning how to do ui updates

--- app/src/main/java/com/example/meteo_android/MainActivity.kt
+++ app/src/main/java/com/example/meteo_android/MainActivity.kt
@@ -18,6 +18,8 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -30,6 +32,7 @@ import kotlinx.coroutines.withContext
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import java.net.URL
+import kotlin.random.Random
 
 
 @Serializable
@@ -68,11 +71,40 @@ data class CityForecast(
 class MainActivity : ComponentActivity() {
     private var cityForecast: CityForecast? = null;
 
+    private val cTemp = mutableStateOf(-999.0)
+
     private suspend fun fetchData() {
         withContext(Dispatchers.IO) {
             try {
                 val response = URL("http://10.0.2.2:8000/api/v1/forecast/cities?lat=56.8750&lon=23.8658&radius=10").readText()
                 cityForecast = Json.decodeFromString<CityForecast>(response)
+
+                var tVal: Double = cTemp.value
+                if ((cityForecast?.hourly_forecast?.size ?: 0) > 0) {
+                    tVal = cityForecast?.hourly_forecast?.get(0)?.vals?.get(1) ?: tVal
+                }
+                cTemp.value = tVal
+            } catch (e: Exception) {
+                // https://stackoverflow.com/questions/67771324/kotlin-networkonmainthreadexception-error-when-trying-to-run-inetaddress-isreac
+                println(e)
+                println(e.message)
+                cityForecast = null
+            } finally { }
+        }
+    }
+
+    private suspend fun fetchTestData() {
+        withContext(Dispatchers.IO) {
+            try {
+                val randTemp = String.format("%.1f", Random.nextInt(60)-30+Random.nextDouble())
+                val response = URL("http://10.0.2.2:8000/api/v1/forecast/test_ctemp?temp=$randTemp").readText()
+                cityForecast = Json.decodeFromString<CityForecast>(response)
+
+                var tVal: Double = cTemp.value
+                if ((cityForecast?.hourly_forecast?.size ?: 0) > 0) {
+                    tVal = cityForecast?.hourly_forecast?.get(0)?.vals?.get(1) ?: tVal
+                }
+                cTemp.value = tVal
             } catch (e: Exception) {
                 // https://stackoverflow.com/questions/67771324/kotlin-networkonmainthreadexception-error-when-trying-to-run-inetaddress-isreac
                 println(e)
@@ -85,94 +117,102 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         runBlocking {
-            fetchData()
+            fetchTestData()
         }
         enableEdgeToEdge()
         setContent {
             Meteo_androidTheme {
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
+                    onClick = {
+                        runBlocking {
+                            fetchTestData()
+                        }
+
+                        var cTemp: Double = -999.0
+                        if ((cityForecast?.hourly_forecast?.size ?: 0) > 0) {
+                            cTemp = cityForecast?.hourly_forecast?.get(0)?.vals?.get(1) ?: cTemp
+                        }
+                        Log.d("DEBUG", "CLICKED ($cTemp)!")
+                    }
                 ) {
                     AllForecasts(cityForecast)
                 }
             }
         }
     }
-}
 
-@Composable
-fun AllForecasts(data: CityForecast?, modifier: Modifier = Modifier) {
-    Column( // TODO: how does scrolling work? - looks like this caps me to a single screen (?)
-        modifier = modifier
-            .padding(8.dp)
-            .background(Color.Red)
-            .verticalScroll(rememberScrollState())
-    ) {
-        Row(
+
+    @Composable
+    fun AllForecasts(data: CityForecast?, modifier: Modifier = Modifier) {
+        Column(
             modifier = modifier
                 .padding(8.dp)
-                .background(Color.Magenta)
+                .background(Color.Red)
+                .verticalScroll(rememberScrollState())
         ) {
-            CurrentTemp(data)
-        }
-        Row {
-            Text(
-                text = "24h weather",
-                fontSize = 60.sp,
-                lineHeight = 120.sp,
-                modifier = Modifier
+            Row(
+                modifier = modifier
                     .padding(8.dp)
-                    .fillMaxWidth(1.0f)
-                    .background(Color.Yellow)
-            )
-        }
-        Column (
-            modifier = modifier
-                .padding(8.dp)
-                .background(Color.Cyan)
-        ) {
-            DailyForecasts(data)
+                    .background(Color.Magenta)
+            ) {
+                CurrentTemp()
+            }
+            Row {
+                Text(
+                    text = "24h weather",
+                    fontSize = 60.sp,
+                    lineHeight = 120.sp,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(1.0f)
+                        .background(Color.Yellow)
+                )
+            }
+            Column (
+                modifier = modifier
+                    .padding(8.dp)
+                    .background(Color.Cyan)
+            ) {
+                DailyForecasts(data)
+            }
         }
     }
-}
 
-@Composable
-fun CurrentTemp(data: CityForecast?) {
-    var cTemp: Double = -999.0
-    if ((data?.hourly_forecast?.size ?: 0) > 0) {
-        cTemp = data?.hourly_forecast?.get(0)?.vals?.get(1) ?: cTemp
+    @Composable
+    fun CurrentTemp() {
+        val showTemp by cTemp
+        Text(
+            text = "$showTemp°",
+            fontSize = 100.sp,
+            lineHeight = 300.sp,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .fillMaxWidth(1.0f)
+                .background(Color.Green)
+        )
     }
 
-    Text(
-        text = "$cTemp°",
-        fontSize = 100.sp,
-        lineHeight = 300.sp,
-        textAlign = TextAlign.Center,
-        modifier = Modifier
-            .fillMaxWidth(1.0f)
-            .background(Color.Green)
-    )
-}
+    @Composable
+    fun DailyForecasts(data: CityForecast?) {
+        val dData: List<Forecast> = data?.daily_forecast ?: emptyList()
 
-@Composable
-fun DailyForecasts(data: CityForecast?) {
-    val dData: List<Forecast> = data?.daily_forecast ?: emptyList()
+        for (d in dData) {
+            val minTemp: Double = d.vals[3]
+            val maxTemp: Double = d.vals[2]
 
-    for (d in dData) {
-        val minTemp: Double = d.vals[3]
-        val maxTemp: Double = d.vals[2]
-
-        Row {
-            Text(
-                text = "$minTemp° - $maxTemp°",
-                fontSize = 40.sp,
-                lineHeight = 80.sp,
-                modifier = Modifier
-                    .padding(8.dp)
-                    .fillMaxWidth(1.0f)
-                    .background(Color.Yellow)
-            )
+            Row {
+                Text(
+                    text = "$minTemp° - $maxTemp°",
+                    fontSize = 40.sp,
+                    lineHeight = 80.sp,
+                    modifier = Modifier
+                        .padding(8.dp)
+                        .fillMaxWidth(1.0f)
+                        .background(Color.Yellow)
+                )
+            }
         }
     }
 }
