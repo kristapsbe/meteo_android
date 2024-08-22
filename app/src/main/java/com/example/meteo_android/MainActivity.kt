f560012703fe3b8d674f51c028ae17f67dcaaa5b
Kristaps <kbebris+git@gmail.com>
2024-08-22 20:13:31 +0300

    trying out notifications

--- app/src/main/java/com/example/meteo_android/MainActivity.kt
+++ app/src/main/java/com/example/meteo_android/MainActivity.kt
@@ -95,7 +95,7 @@ class MainActivity : ComponentActivity() {
                     val randTemp = String.format("%.1f", Random.nextInt(60)-30+Random.nextDouble())
 
                     var urlString = "http://10.0.2.2:8000/api/v1/forecast/test_ctemp?temp=$randTemp"
-                    urlString = "http://10.0.2.2:8000/api/v1/forecast/cities?lat=$lat&lon=$lon&radius=10"
+                    //urlString = "http://10.0.2.2:8000/api/v1/forecast/cities?lat=$lat&lon=$lon&radius=10"
 
                     val response = URL(urlString).readText()
                     openFileOutput(responseFname, MODE_PRIVATE).use { fos ->
