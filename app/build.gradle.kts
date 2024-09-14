3b1d202fd317a0dd2eb77f8a7132a1e5542c4d82
Kristaps <kbebris+git@gmail.com>
2024-09-14 11:04:13 +0300

    small improvements to the widgets layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 4
-        versionName = "0.1.1"
+        versionCode = 5
+        versionName = "0.1.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
