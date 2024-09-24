e2c5de376b124a5006a8d3782929db618dc4eacf
Kristaps <kbebris+git@gmail.com>
2024-09-24 22:03:04 +0300

    updated widget rain text

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 52
-        versionName = "0.5.13"
+        versionCode = 53
+        versionName = "0.5.14"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
