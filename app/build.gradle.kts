39cc7021a0976cb034731722180c65d6350372e1
Kristaps <kbebris+git@gmail.com>
2024-09-30 19:55:28 +0300

    new experimental widget layout

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 80
-        versionName = "0.8.11"
+        versionCode = 81
+        versionName = "0.8.12"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
