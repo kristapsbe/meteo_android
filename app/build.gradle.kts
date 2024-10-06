a4a0521c78f4fb02303d0d5f0ea5f6af50fd07ee
Kristaps <kbebris+git@gmail.com>
2024-10-07 00:00:12 +0300

    widget fix

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 107
-        versionName = "0.9.23"
+        versionCode = 108
+        versionName = "0.9.24"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
