5ae604e4988061b53f985bebb6b1ad3756155103
Kristaps <kbebris+git@gmail.com>
2024-12-02 22:09:13 +0200

    version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 26
         targetSdk = 34
-        versionCode = 146
-        versionName = "0.9.62"
+        versionCode = 147
+        versionName = "0.9.63"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
