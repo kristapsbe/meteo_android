b3809517150a0a7d5456e830d33b9cc321c18b0e
Kristaps <kbebris+git@gmail.com>
2025-12-30 12:37:08 +0200

    version bump

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "lv.kristapsbe.meteo_android"
         minSdk = 28
         targetSdk = 36
-        versionCode = 162
-        versionName = "1.0.14"
+        versionCode = 163
+        versionName = "1.0.15"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
