a22e57caa32b913b967d4a6a28461b463fcc07fd
Kristaps <kbebris+git@gmail.com>
2026-03-09 20:11:16 +0200

    updating more deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,6 @@
 plugins {
     alias(libs.plugins.android.application) apply false
     alias(libs.plugins.jetbrains.kotlin.android) apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.0.0" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.10" apply false
     alias(libs.plugins.compose.compiler) apply false
 }
